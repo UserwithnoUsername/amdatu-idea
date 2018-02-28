f07767c8cfc721e38adbee8413aefb7513c1b251
Bram Pouwelse <bram@pouwelse.com>
2018-02-28 11:17:59 +0100

    Import org.jetbrains.osgi.bnd.run package from osmorc plugin

---
+++ src/main/java/org/amdatu/ide/run/BndRunConfigurationBase.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2000-2017 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.amdatu.ide.run;
+
+import com.intellij.execution.ExecutionException;
+import com.intellij.execution.Executor;
+import com.intellij.execution.configurations.*;
+import com.intellij.execution.runners.ExecutionEnvironment;
+import com.intellij.execution.util.JavaParametersUtil;
+import com.intellij.openapi.components.PersistentStateComponent;
+import com.intellij.openapi.options.SettingsEditor;
+import com.intellij.openapi.project.Project;
+
+import org.amdatu.ide.i18n.OsmorcBundle;
+import org.jdom.Element;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.osgi.bnd.run.BndRunConfigurationOptions;
+
+import java.io.File;
+
+public abstract class BndRunConfigurationBase extends LocatableConfigurationBase implements ModuleRunProfile, PersistentStateComponent<Element> {
+  public BndRunConfigurationBase(Project project, @NotNull ConfigurationFactory factory, String name) {
+    super(project, factory, name);
+  }
+
+  @Override
+  protected Class<BndRunConfigurationOptions> getOptionsClass() {
+    return BndRunConfigurationOptions.class;
+  }
+
+  @Override
+  protected BndRunConfigurationOptions getOptions() {
+    return (BndRunConfigurationOptions)super.getOptions();
+  }
+
+  @Override
+  public Element getState() {
+    Element element = new Element("state");
+    super.writeExternal(element);
+    return element;
+  }
+
+  @NotNull
+  @Override
+  public SettingsEditor<? extends BndRunConfigurationBase> getConfigurationEditor() {
+    return new BndRunConfigurationEditor(getProject());
+  }
+
+  @Nullable
+  @Override
+  public abstract RunProfileState getState(@NotNull Executor executor, @NotNull ExecutionEnvironment environment) throws ExecutionException;
+
+  @Override
+  public void checkConfiguration() throws RuntimeConfigurationException {
+    String file = getOptions().getBndRunFile();
+    if (file == null || !new File(file).isFile()) {
+      throw new RuntimeConfigurationException(OsmorcBundle.message("bnd.run.configuration.invalid", file));
+    }
+    if (getOptions().getUseAlternativeJre()) {
+      JavaParametersUtil.checkAlternativeJRE(getOptions().getAlternativeJrePath());
+    }
+  }
+
+  public static class Launch extends BndRunConfigurationBase {
+    public Launch(Project project, @NotNull ConfigurationFactory factory, String name) {
+      super(project, factory, name);
+    }
+
+    @Nullable
+    @Override
+    public BndLaunchState getState(@NotNull Executor executor, @NotNull ExecutionEnvironment environment) throws ExecutionException {
+      return new BndLaunchState(environment, this);
+    }
+  }
+
+  public static class Test extends BndRunConfigurationBase {
+    public Test(Project project, @NotNull ConfigurationFactory factory, String name) {
+      super(project, factory, name);
+    }
+
+    @Nullable
+    @Override
+    public BndTestState getState(@NotNull Executor executor, @NotNull ExecutionEnvironment environment) throws ExecutionException {
+      return new BndTestState(environment, this);
+    }
+  }
+}
---
+++ src/main/java/org/amdatu/ide/run/BndRunConfigurationProducer.java
@@ -0,0 +1,78 @@
+// Copyright 2000-2017 JetBrains s.r.o.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package org.amdatu.ide.run;
+
+import com.intellij.execution.Location;
+import com.intellij.execution.actions.ConfigurationContext;
+import com.intellij.execution.actions.RunConfigurationProducer;
+import com.intellij.execution.configurations.ConfigurationFactory;
+import com.intellij.openapi.util.Ref;
+import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiElement;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.osgi.bnd.BndFileType;
+
+public abstract class BndRunConfigurationProducer extends RunConfigurationProducer<BndRunConfigurationBase> {
+  protected BndRunConfigurationProducer(@NotNull ConfigurationFactory factory) {
+    super(factory);
+  }
+
+  @Override
+  protected boolean setupConfigurationFromContext(BndRunConfigurationBase configuration, ConfigurationContext context, Ref<PsiElement> source) {
+    Location location = context.getLocation();
+    if (location != null) {
+      VirtualFile file = location.getVirtualFile();
+      if (file != null && !file.isDirectory()) {
+        String extension = file.getExtension();
+        if (BndFileType.BND_RUN_EXT.equals(extension) || BndFileType.BND_EXT.equals(extension)) {
+          Boolean hasTestCases = BndLaunchUtil.hasTestCases(file.getPath());
+          if (hasTestCases == Boolean.FALSE && configuration instanceof BndRunConfigurationBase.Launch ||
+              hasTestCases == Boolean.TRUE && configuration instanceof BndRunConfigurationBase.Test) {
+            configuration.setName(context.getModule().getName());
+            configuration.getOptions().setBndRunFile(file.getPath());
+            return true;
+          }
+        }
+      }
+    }
+
+    return false;
+  }
+
+  @Override
+  public boolean isConfigurationFromContext(BndRunConfigurationBase configuration, ConfigurationContext context) {
+    if (getConfigurationFactory() == configuration.getFactory()) {
+      Location location = context.getLocation();
+      if (location != null) {
+        VirtualFile file = location.getVirtualFile();
+        return file != null && !file.isDirectory() && FileUtil.pathsEqual(file.getPath(), configuration.getOptions().getBndRunFile());
+      }
+    }
+
+    return false;
+  }
+
+  public static class Launch extends BndRunConfigurationProducer {
+    public Launch() {
+      super(BndRunConfigurationType.getInstance().getConfigurationFactories()[0]);
+    }
+  }
+
+  public static class Test extends BndRunConfigurationProducer {
+    public Test() {
+      super(BndRunConfigurationType.getInstance().getConfigurationFactories()[1]);
+    }
+  }
+}
