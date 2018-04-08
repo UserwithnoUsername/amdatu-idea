cc100c4ffb7445bfe3a0325d8206d7dc27b762a6
Bram Pouwelse <bram@pouwelse.com>
2018-04-08 20:07:56 +0200

    Some final tweaks to the new repositories panel. Removed the initial version. The new version is written in Kotlin and uses the layout builder instead of the xml + java form stuff.

--- src/main/java/org/amdatu/ide/toolwindow/RepositoriesPanel.kt
+++ src/main/java/org/amdatu/ide/toolwindow/RepositoriesPanel.kt
@@ -2,6 +2,7 @@ package org.amdatu.ide.toolwindow
 
 import aQute.bnd.service.RepositoryPlugin
 import aQute.bnd.version.Version
+import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.project.Project
 import com.intellij.ui.ColoredTreeCellRenderer
 import com.intellij.ui.layout.CCFlags
@@ -9,6 +10,7 @@ import com.intellij.ui.layout.panel
 import com.intellij.ui.treeStructure.Tree
 import com.intellij.util.ui.tree.AbstractTreeModel
 import org.amdatu.ide.AmdatuIdePlugin
+import org.amdatu.ide.WorkspaceRefreshedNotifier
 import javax.swing.*
 import javax.swing.tree.DefaultMutableTreeNode
 import javax.swing.tree.TreePath
@@ -16,30 +18,39 @@ import javax.swing.tree.TreePath
 class RepositoriesPanel(private val myProject: Project) {
 
     fun createRepositoriesPanel(): JPanel {
-        val searchField = JTextField("test")
+        val searchField = JTextField()
+        val repositoriesTreeModel = RepositoriesTreeModel(myProject, searchField)
+        val repositoriesTree = Tree(repositoriesTreeModel)
 
-
-
-        val repositoriesTree = Tree(RepositoriesTreeModel(myProject, searchField))
-
-        searchField.addActionListener { e ->
+        searchField.addActionListener { _ ->
             run {
                 repositoriesTree.updateUI()
             }
         }
 
-        repositoriesTree.isEditable = false
-        repositoriesTree.isRootVisible = false
-        repositoriesTree.cellRenderer = object: ColoredTreeCellRenderer() {
-            override fun customizeCellRenderer(tree: JTree, value: Any?, selected: Boolean, expanded: Boolean, leaf: Boolean, row: Int, hasFocus: Boolean) {
-                when (value) {
-                    is RepositoryPlugin -> append(value.name)
-                    is BsnWithRepoRef -> append(value.bsn)
-                    else -> append(value.toString())
+        repositoriesTree.apply {
+            isEditable = false
+            isRootVisible = false
+            cellRenderer = object : ColoredTreeCellRenderer() {
+                override fun customizeCellRenderer(tree: JTree, value: Any?, selected: Boolean, expanded: Boolean, leaf: Boolean, row: Int, hasFocus: Boolean) {
+                    when (value) {
+                        is RepositoryPlugin -> append(value.name)
+                        is BsnWithRepoRef -> append(value.bsn)
+                        else -> append(value.toString())
+                    }
                 }
             }
-
         }
+
+        val messageBusConnection = myProject.messageBus.connect()
+        messageBusConnection.subscribe(WorkspaceRefreshedNotifier.WORKSPACE_REFRESHED, WorkspaceRefreshedNotifier {
+
+            ProgressManager.getInstance().runProcess({
+                repositoriesTreeModel.refreshRepositories()
+                repositoriesTree.updateUI()
+            }, null)
+
+        })
 
         return panel {
             row {
@@ -51,28 +62,38 @@ class RepositoriesPanel(private val myProject: Project) {
             row {
                 panel {
                     row {
-                        JScrollPane(repositoriesTree)(CCFlags.growY)
+                        JScrollPane(repositoriesTree)(CCFlags.growX, CCFlags.pushX, CCFlags.growY, CCFlags.pushY)
                     }
-                }()
+                }(CCFlags.growX, CCFlags.pushX, CCFlags.growY, CCFlags.pushY)
             }
         }
-
     }
 
     data class BsnWithRepoRef(val bsn: String, val repositoryPlugin: RepositoryPlugin)
 
-    class RepositoriesTreeModel(private val myProject: Project, private val mySearchField: JTextField) : AbstractTreeModel() {
-        private val repositoryPlugins: List<RepositoryPlugin>
+    class RepositoriesTreeModel(val myProject: Project, private val mySearchField: JTextField) : AbstractTreeModel() {
+        private val repositoryPlugins: MutableList<RepositoryPlugin> = mutableListOf()
         private val repoBundlesCache = mutableMapOf<RepositoryPlugin, List<BsnWithRepoRef>>()
         private val myRoot = DefaultMutableTreeNode(0)
 
         init {
-            val workspace = myProject.getComponent(AmdatuIdePlugin::class.java)?.workspace
-            repositoryPlugins = workspace
-                    ?.getPlugins(RepositoryPlugin::class.java)
-                    ?.sortedBy { repo -> repo.name } ?: emptyList()
+//            val workspace = myProject.getComponent(AmdatuIdePlugin::class.java)?.workspace
+//            repositoryPlugins = workspace
+//                    ?.getPlugins(RepositoryPlugin::class.java)
+//                    ?.sortedBy { repo -> repo.name }
+//                    ?.toMutableList() ?: mutableListOf()
+            refreshRepositories();
         }
 
+        fun refreshRepositories() {
+            val workspace = myProject.getComponent(AmdatuIdePlugin::class.java)?.workspace
+            repositoryPlugins.clear()
+            repoBundlesCache.clear()
+            repositoryPlugins.addAll(workspace
+                    ?.getPlugins(RepositoryPlugin::class.java)
+                    ?.sortedBy { repo -> repo.name }
+                    ?.toMutableList() ?: mutableListOf())
+        }
 
         override fun getChild(parent: Any?, index: Int): Any {
             return when {
@@ -89,7 +110,7 @@ class RepositoriesPanel(private val myProject: Project) {
             }
         }
 
-        private fun bsnMatchesFilter(bsn: String) : Boolean {
+        private fun bsnMatchesFilter(bsn: String): Boolean {
             val text = mySearchField.text
             if (text == null || text.isBlank()) {
                 return true
@@ -100,11 +121,7 @@ class RepositoriesPanel(private val myProject: Project) {
             } else {
                 bsn.startsWith(text)
             }
-
-
         }
-
-
 
         override fun getRoot(): Any {
             return myRoot
@@ -149,8 +166,5 @@ class RepositoriesPanel(private val myProject: Project) {
                 else -> -1
             }
         }
-
-
     }
-
 }
