384235676f92302b3b58cc969ff29578171358a4
Bram Pouwelse <bram@pouwelse.com>
2018-04-26 15:17:40 +0200

    AMDATUIDEA-9 Report plugin conflict when Osmorc is enabled

--- src/main/java/org/amdatu/idea/AmdatuIdeaPluginStartup.java
+++ src/main/java/org/amdatu/idea/AmdatuIdeaPluginStartup.java
@@ -14,16 +14,35 @@
 
 package org.amdatu.idea;
 
+import com.intellij.diagnostic.errordialog.PluginConflictDialog;
+import com.intellij.ide.plugins.PluginManager;
+import com.intellij.openapi.extensions.PluginId;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
 
 public class AmdatuIdeaPluginStartup implements StartupActivity {
 
     @Override
     public void runActivity(@NotNull Project project) {
+        checkOsmorcNotActive();
+
         AmdatuIdeaPlugin amdatuIdeaPlugin = project.getComponent(AmdatuIdeaPlugin.class);
         amdatuIdeaPlugin.getWorkspace();
+    }
+
+    // Using this plugin with Osmorc enabled leads to unpredictable results let the user decide which one should be active
+    private void checkOsmorcNotActive() {
+        PluginId osmorcPluginId = PluginId.findId("Osmorc");
+        PluginId amdatuPluginId = PluginId.findId("org.amdatu.idea");
+
+        if (osmorcPluginId != null && PluginManager.getPlugin(osmorcPluginId).isEnabled()
+                && PluginManager.getPlugin(amdatuPluginId).isEnabled()) {
+
+            new PluginConflictDialog(Arrays.asList(amdatuPluginId, osmorcPluginId), false).show();
+        }
     }
 
 }
